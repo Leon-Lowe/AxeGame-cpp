275599fbfac79cd37147d2cf08364dec22855d07
Leon Lowe <leonlowe0@outlook.com>
2022-08-16 14:32:59 +0100

    Axe
    Added the following:
     - A rectangle to represent the Axe in the game
     - Axe moves down indefinitely

--- Axe-Game/main.cpp
+++ Axe-Game/main.cpp
@@ -9,7 +9,7 @@
 int main()
 {
     //Window Variables
-    const int SCREEN_WIDTH = 1080;
+    const int SCREEN_WIDTH = 1280;
     const int SCREEN_HEIGHT = 720;
     const char* WINDOW_NAME = "Axe Game";
     const Color WINDOW_COLOUR = GRAY;
@@ -17,11 +17,19 @@ int main()
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME);
 
     //Circle Variables
-    int circleX = SCREEN_WIDTH / 2;
+    int circleX = 55;
     int circleY = SCREEN_HEIGHT / 2;
-    int circleRadius = 50;
+    int CIRCLE_RADIUS = 50;
     const Color CIRCLE_COLOUR = BLUE;
     const int CIRCLE_SPEED = 3;
+
+    //Axe Variables
+    int rectX = SCREEN_WIDTH / 2;
+    int rectY = 0;
+    const int RECT_WIDTH = 100;
+    const int RECT_HEIGHT = 50;
+    const Color RECT_COLOUR = RED;
+    const int RECT_SPEED = 5;
 
     //Main Game Loop
     bool running = true;
@@ -39,24 +47,28 @@ int main()
         ClearBackground(WINDOW_COLOUR);
 
         //Begin Game Logic
-        DrawCircle(circleX, circleY, circleRadius, CIRCLE_COLOUR);
+        DrawCircle(circleX, circleY, CIRCLE_RADIUS, CIRCLE_COLOUR);
+        DrawRectangle(rectX, rectY, RECT_WIDTH, RECT_HEIGHT, RECT_COLOUR);
+
+        //Move Axe
+        rectY += (1 * RECT_SPEED);
         
         //Horizontal Movement
-        if (IsKeyDown(KEY_D) && circleX <= (SCREEN_WIDTH - circleRadius))
+        if (IsKeyDown(KEY_D) && circleX <= (SCREEN_WIDTH - CIRCLE_RADIUS))
         {
             circleX += (1 * CIRCLE_SPEED);
         }
-        if (IsKeyDown(KEY_A) && circleX >= (0 + circleRadius))
+        if (IsKeyDown(KEY_A) && circleX >= (0 + CIRCLE_RADIUS))
         {
             circleX += (-1 * CIRCLE_SPEED);
         }
 
         //Vertical Movement
-        if (IsKeyDown(KEY_W) && circleY >= (0 + circleRadius))
+        if (IsKeyDown(KEY_W) && circleY >= (0 + CIRCLE_RADIUS))
         {
             circleY += (-1 * CIRCLE_SPEED);
         }
-        if (IsKeyDown(KEY_S) && circleY <= (SCREEN_HEIGHT - circleRadius))
+        if (IsKeyDown(KEY_S) && circleY <= (SCREEN_HEIGHT - CIRCLE_RADIUS))
         {
             circleY += (1 * CIRCLE_SPEED);
         }
