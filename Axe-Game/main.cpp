e4392ae1e5e838f43e9638f188d7b09e277c8fb3
Leon Lowe <leonlowe0@outlook.com>
2022-08-16 14:15:01 +0100

    Circle Character
    Added the following:
     - Create a circle in the centre of the screen
     - Allow player to move the circle with the 'WASD' keys
     - Lock the FPS to 60

--- Axe-Game/main.cpp
+++ Axe-Game/main.cpp
@@ -1,19 +1,67 @@
+/*
+    * main.cpp
+    * Create a game where 
+    * Created by Leon Lowe
+*/
+
 #include "raylib.h"
 
 int main()
 {
+    //Window Variables
     const int SCREEN_WIDTH = 1080;
     const int SCREEN_HEIGHT = 720;
     const char* WINDOW_NAME = "Axe Game";
+    const Color WINDOW_COLOUR = GRAY;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_NAME);
 
+    //Circle Variables
+    int circleX = SCREEN_WIDTH / 2;
+    int circleY = SCREEN_HEIGHT / 2;
+    int circleRadius = 50;
+    const Color CIRCLE_COLOUR = BLUE;
+    const int CIRCLE_SPEED = 3;
+
+    //Main Game Loop
     bool running = true;
+
+    SetTargetFPS(60);
 
     while(running)
     {
+        if(WindowShouldClose() == true)
+        {
+            running = false;
+        }
+
         BeginDrawing();
-        ClearBackground(GRAY);
+        ClearBackground(WINDOW_COLOUR);
+
+#pragma region Game Logic
+        DrawCircle(circleX, circleY, circleRadius, CIRCLE_COLOUR);
+
+        //Horizontal Movement
+        if (IsKeyDown(KEY_D))
+        {
+            circleX += (1 * CIRCLE_SPEED);
+        }
+        if (IsKeyDown(KEY_A))
+        {
+            circleX += (-1 * CIRCLE_SPEED);
+        }
+
+        //Vertical Movement
+        if (IsKeyDown(KEY_W))
+        {
+            circleY += (-1 * CIRCLE_SPEED);
+        }
+        if (IsKeyDown(KEY_S))
+        {
+            circleY += (1 * CIRCLE_SPEED);
+        }
+#pragma endregion
+
         EndDrawing();
     }
 
